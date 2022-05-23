f2b8c95bf981f99c92e0a10ceb42bc676d22c0f7
cnavarroe <cnavarroe@utem.cl>
2022-05-23 01:27:09 +0000

    primer avance

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".numeros").innerHTML = generateNumber();
+  console.log("Hello Rigo from the console! ");
 };
+
+let generateNumber = () => {
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  numeros[11] = "J";
+  numeros[12] = "Q";
+  numeros[13] = "K";
+  let seleccion = Math.floor(Math.random() * numeros.length);
+  return seleccion;
+};
+
+//let iconos = ["&diams;", "&hearts;", "&clubs;", "&spades;"];
